b5279a78bc25fbe4ab78957f1ef62a1bfa55abae
DmytroTarasiuk <dmytro.tarasiuk1899@gmail.com>
2021-06-26 13:16:53 +0200

    Dark Mode and filter functionality

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 
 const API_URL = 'https://restcountries.eu/rest/v2/all'
 const SEARCH_API ='https://restcountries.eu/rest/v2/name/'
+const EUROPE_API = 'https://restcountries.eu/rest/v2/region/europe'
+const AFRICA_API = 'https://restcountries.eu/rest/v2/region/africa'
+const AMERICAS_API = 'https://restcountries.eu/rest/v2/region/americas'
+const ASIA_API = 'https://restcountries.eu/rest/v2/region/asia'
+const OCEANIA_API = 'https://restcountries.eu/rest/v2/region/oceania'
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 const countryContainer = document.getElementById('country-container')
 const countryInfo = document.getElementById('country-information')
 const main = document.getElementById('main')
+const selectEl = document.getElementById('region')
+const darkMode = document.getElementById('dark')
+const navEl = document.querySelector('.nav')
+const btnEl = document.querySelector('.btn')
 
 getCountries(API_URL)
 
@@ -79,4 +88,31 @@ form.addEventListener('submit', (e) => {
         window.location.reload()
     }
 })
+
+selectEl.addEventListener('change', (e) => {
+    const userChoice = e.target.value
+    if(userChoice === 'europe') {
+        getCountries(EUROPE_API)
+    } else if(userChoice === 'africa') {
+        getCountries(AFRICA_API)
+    } else if(userChoice ==='asia') {
+        getCountries(ASIA_API)
+    } else if(userChoice === 'americas') {
+        getCountries(AMERICAS_API)
+    } else if(userChoice === 'oceania') {
+        getCountries(OCEANIA_API)
+    } else {
+        getCountries(API_URL)
+    }
+})
+
+darkMode.addEventListener('click', () => {
+    navEl.classList.toggle('dark')
+    main.classList.toggle('dark')
+    selectEl.classList.toggle('dark')
+    search.classList.toggle('dark')
+    btnEl.classList.toggle('dark')
+})
+
+
 
